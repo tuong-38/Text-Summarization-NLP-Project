9710838cd3123bdce97df825870da5ea4ad7aace
PHAM LE THIEN TUONG <TuongPLT.B25DCTV077@stu.ptit.edu.vn>
2026-08-01 11:18:57 +0700

    feat: complete Stage 01 - Data Ingestion pipeline

---
+++ src/textSummarizer/entity/config_entity.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+
+
+# Thêm Dataclass cho Stage 2
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
+# Thêm Dataclass này cho Stage 3
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    tokenizer_name: Path
